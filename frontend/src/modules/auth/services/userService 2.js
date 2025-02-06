4250142ef18ba10981340958ae3f41208c8d7723
nazariegode <dnazariego@gmail.com>
2025-02-06 16:30:33 +0000

    cambios

---
+++ frontend/src/modules/auth/services/userService 2.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const API_BASE = process.env.REACT_APP_API_BASE;
+
+export const updateUser = async (userData) => {
+    try {
+        const response = await axios.put(`${API_BASE}/users`, userData);
+        return response.data
+    } catch (error) {
+        console.log("error", error)
+    }
+};
+
+export const deleteUser = async (userId) => {
+    try {
+        const response = await axios.delete(`${API_BASE}/users/${userId}`,);
+        return response.data
+    } catch (error) {
+        console.log("error", error)
+    }
+};
