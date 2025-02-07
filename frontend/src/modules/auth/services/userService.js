755bae39cab861a46e26636d395f3b6cfa7b9295
XCODE89 <maph0989@gmail.com>
2025-02-07 12:06:14 -0300

    + Alertas en actualizacion de datos

--- frontend/src/modules/auth/services/userService.js
+++ frontend/src/modules/auth/services/userService.js
@@ -5,9 +5,10 @@ const API_BASE = process.env.REACT_APP_API_BASE;
 export const updateUser = async (userData) => {
     try {
         const response = await axios.put(`${API_BASE}/users`, userData);
-        return response.data
+        return response
     } catch (error) {
         console.log("error", error)
+        return (error)
     }
 };
 
